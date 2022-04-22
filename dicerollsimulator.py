17d61496dccc124c033c0c3851b751e1f2863e13
MyNameIsLogan <97649562+MyNameIsLogan@users.noreply.github.com>
2022-04-21 22:09:47 -0400

    Created D2 and D6 dice options
    Here, we added the two-sided and six-sided (standard) dice which users can enter.

--- dicerollsimulator.py
+++ dicerollsimulator.py
@@ -2,3 +2,8 @@ import random
 
 inp_dice_type= input("What kind of dice would you like to roll? (Options: D2, D6, D8, D10, D20): ").lower()
 
+if inp_dice_type== "D2":
+    print(random.randint(1, 2))
+
+if inp_dice_type== "D6":
+    print(random.randint(1, 6))  
